faaf1499954799a9a7233351725dc67ba2bc5966
Esteban Flores <ebflores88@gmail.com>
2020-02-12 04:05:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ function add(a, b){
   return a + b
 }
 
-function subtract(){
+function subtract(a, b){
 
 }
 
-function multiply(){
+function multiply(a, b){
 
 }
 
-function divide(){
+function divide(a, b){
 
 }
 
