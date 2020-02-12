a29db874191418997a0b297efabffdf7340412cc
Esteban Flores <ebflores88@gmail.com>
2020-02-12 04:00:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,5 @@
-var number = 10
 
-function add5(){
-  number += 5
+
+function makeInt(n){
+  parseInt(number, 10)
 }
-
-function divideBy3(){
-  number /= 3
-}
-
-dividBy3()
-
-console.log(number)
-
-number = 10
-
-add5()
-
-console.log(number)
-
-divideBy3()
-
-console.log(number)
-
